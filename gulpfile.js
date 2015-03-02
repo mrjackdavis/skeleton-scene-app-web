67e2be65e02f3ce07484ea8d3a57266cf55232bb
Jack Davis <mr.jackdavis@gmail.com>
2015-03-02 16:23:36 +1100

    Setup basic html server for development

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
+var connect = require('gulp-connect');
 
 gulp.task('default', function() {
-	
+
+});
+
+gulp.task('start', function() {
+	connect.server({
+		root: './',
+		livereload: true
+	});
 });
