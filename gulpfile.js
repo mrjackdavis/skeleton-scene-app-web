8a881c0378e7401fa5c8dc55fd5276f9f37ba8dd
Jack Davis <mr.jackdavis@gmail.com>
2015-03-02 16:52:34 +1100

    Setup less

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var connect = require('gulp-connect');
+var less = require('gulp-less');
 
 gulp.task('default', function() {
 
+});
+
+gulp.task('build', function() {
+	return gulp.src('./less/**')
+		.pipe(less())
+		.pipe(gulp.dest('./css'));
 });
 
 gulp.task('start', function() {
