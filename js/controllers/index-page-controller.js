deb202fc03c3e49d0c9d9c9a67be187724b9527f
Jack Davis <mr.jackdavis@gmail.com>
2015-03-03 11:57:25 +1100

    Sort out nav-bar a bit

--- js/controllers/index-page-controller.js
+++ js/controllers/index-page-controller.js
@@ -1,5 +1,12 @@
 angular.module('SceneSkeleton')
 	.controller('IndexPageController',function($scope){
 		$scope.showTitle = true;
-		$scope.showSceneForm = false;
+		$scope.showPanel = false;
+		$scope.showNavbar = false;
+
+		$scope.openPanel = function(){
+			$scope.showTitle = false;
+			$scope.showNavbar = true;
+			$scope.showPanel = true;
+		}
 	});
