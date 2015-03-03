402f7741780908d81809d135d2d0a950970d51d0
Jack Davis <mr.jackdavis@gmail.com>
2015-03-03 11:38:14 +1100

    Setup routes

--- js/app.js
+++ js/app.js
@@ -1,1 +1,10 @@
-angular.module('SceneSkeleton',[]);
+angular.module('SceneSkeleton',['ngRoute'])
+	.config(['$routeProvider',function($routeProvider){
+		$routeProvider
+			.when('/',{
+				templateUrl: 'templates/scene/create.html'
+			})
+			.otherwise({
+				redirectTo:'/'
+			});
+	}]);
---
+++ js/controllers/index-page-controller.js
@@ -0,0 +1,5 @@
+angular.module('SceneSkeleton')
+	.controller('IndexPageController',function($scope){
+		$scope.showTitle = true;
+		$scope.showSceneForm = false;
+	});
