a8b8a88afd0e561f039e9412383a7dd71736485f
Jack Davis <mr.jackdavis@gmail.com>
2015-03-03 12:20:16 +1100

    Display random image when creating scene

--- js/controllers/scene-create-controller.js
+++ js/controllers/scene-create-controller.js
@@ -1,10 +1,14 @@
 angular.module('SceneSkeleton')
-	.controller('SceneCreateController',function($scope,$location){
+	.controller('SceneCreateController',function($scope,$location,$timeout){
 		$scope.url = '';
 
 		$scope.CreateScene = function CreateScene(url){
-			console.log('updating location to /#/scene/1234');
-			$location.path('/scene/1234');
+			$scope.loading = true;
+			
+			$timeout(function(){
+				console.log('updating location to /#/scene/1234');
+				$location.path('/scene/1234');
+			},2000);
 		}
 	});
 
