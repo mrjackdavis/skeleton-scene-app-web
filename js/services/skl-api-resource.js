e2a2661aad044b91f14cd5feff84d94ab2318c05
Jack Davis <mr.jackdavis@gmail.com>
2015-05-03 19:19:39 +1000

    Update resource to use public url (temporary)

--- js/services/skl-api-resource.js
+++ js/services/skl-api-resource.js
@@ -1,4 +1,4 @@
 angular.module('SceneSkeleton')
 	.factory("Scene", function($resource) {
-		return $resource("http://dockervm:8081/scene/:id");
+		return $resource("http://skl-api-426627428.ap-southeast-2.elb.amazonaws.com/scene/:id");
 	});
