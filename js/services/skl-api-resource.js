daa0c7df93ba8f004d55d56cc5e4c852ff18999c
Jack Davis <mr.jackdavis@gmail.com>
2015-05-09 14:36:38 +1000

    Migrate data transform to skl-api-resource

--- js/services/skl-api-resource.js
+++ js/services/skl-api-resource.js
@@ -1,4 +1,56 @@
 angular.module('SceneSkeleton')
 	.factory("Scene", function($resource) {
-		return $resource("http://skl-api-426627428.ap-southeast-2.elb.amazonaws.com/scene/:id/:dateCreated");
+		var service = $resource("http://skl-api-426627428.ap-southeast-2.elb.amazonaws.com/scene/:id/:dateCreated",{},{
+			get:{
+				transformResponse:function(data,header){
+					var scene = JSON.parse(data);
+					return NormaliseScene(scene);
+				}
+			},
+			query:{
+				isArray: true,
+				transformResponse:function(data,header){
+					var scenes = angular.fromJson(data);
+					console.log(scenes);
+					return scenes.map(NormaliseScene);
+				}
+			}
+		});
+
+		// var resource = angular.extend(service.prototype,{
+		// 	get:function(params,cb){
+		// 		return service.get(params, function(scene){
+		// 				cb(NormaliseScene(scene));
+		// 			});
+		// 	},
+		// });
+
+		return service;
 	});
+
+function NormaliseScene(scene){
+
+	if(!scene.processes){
+		throw new Error('No processes in scene')
+	}
+
+	scene.completeProcesses = scene.processes.filter(function (prcs) {
+		return prcs.status == 'Complete';
+	});
+	scene.runningProcesses = scene.processes.filter(function (prcs) {
+		return prcs.status == 'InProgress';
+	});
+
+	if(scene.processes.length < 1){
+		scene.status = 'Pending';
+	}else{
+		if(scene.completeProcesses.length > 0){
+			scene.status = 'Complete';
+		}else if(scene.runningProcesses.length > 0){
+			scene.status = 'In Progress';
+		}else{
+			scene.status = 'Unknown';
+		}
+	}
+	return scene;
+}
