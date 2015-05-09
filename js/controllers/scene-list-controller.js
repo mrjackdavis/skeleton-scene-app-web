9409459dfdb193cc1d6b9436e754402763a9a293
Jack Davis <mr.jackdavis@gmail.com>
2015-05-09 13:48:09 +1000

    Remove nav and improve scene list

--- js/controllers/scene-list-controller.js
+++ js/controllers/scene-list-controller.js
@@ -2,6 +2,28 @@ angular.module('SceneSkeleton')
 	.controller('SceneListController',function($scope,Scene){
 		$scope.scenes = [];
 		Scene.query(function(data) {
-			$scope.scenes = data;
+			
+			
+			$scope.scenes = data.map(function(scene){
+				if(scene.processes.length < 1){
+					scene.status = 'Pending';
+				}else{
+					var finishedProcesses = scene.processes.filter(function (prcs) {
+						return prcs.status == 'Complete';
+					});
+					var currentProcesses = scene.processes.filter(function (prcs) {
+						return prcs.status == 'InProgress';
+					});
+
+					if(finishedProcesses.length > 0){
+						scene.status = 'Complete';
+					}else if(currentProcesses.length > 0){
+						scene.status = 'In Progress';
+					}else{
+						scene.status = 'Unknown';
+					}
+				}
+				return scene;
+			});
 		});
 	});
