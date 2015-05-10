440896bcca048dc1d6063b655fefac0b43b6fb54
Jack Davis <mr.jackdavis@gmail.com>
2015-05-10 14:14:57 +1000

    Display list of scenes in grid

--- js/controllers/scene-list-controller.js
+++ js/controllers/scene-list-controller.js
@@ -2,6 +2,24 @@ angular.module('SceneSkeleton')
 	.controller('SceneListController',function($scope,Scene){
 		$scope.scenes = [];
 		Scene.query(function(data) {
-			$scope.scenes = data;
+			$scope.rows = [];
+			$scope.totalScenes = data.length;
+
+			var scenes =  data.sort(function(a,b){
+				return a.dateCreated < b.dateCreated;
+			});
+
+			var currentRow = [];
+
+			scenes.forEach(function(scene){
+				if(currentRow.length >= 3){
+					$scope.rows.push(currentRow);
+					currentRow = [];
+				}
+				currentRow.push(scene);
+			});
+			if(currentRow.length > 0){
+				$scope.rows.push(currentRow);
+			}
 		});
 	});
