017854b409b51b8dcc33fb88041bc8f0d75e8ee7
Jack Davis <mr.jackdavis@gmail.com>
2015-05-18 16:31:34 +1000

    Sort out generator selection in create route

--- js/controllers/scene-create-controller.js
+++ js/controllers/scene-create-controller.js
@@ -2,13 +2,16 @@ angular.module('SceneSkeleton')
 	.controller('SceneCreateController',function($scope,$location,$timeout,SceneRequests,Generators){
 		$scope.url = '';
 		$scope.Generators = Generators.query();
+		$scope.SelectedGenerator = $scope.Generators[0];
+
 
 		$scope.CreateScene = function CreateScene(url){
 			$scope.loading = true;
 
 			SceneRequests.save({
 				resourceType:"URL",
-				resourceURI:$scope.url
+				resourceURI:$scope.url,
+				generatorName:$scope.SelectedGenerator
 			}).$promise.then(function(data){
 				$location.path('/scene');
 			});
