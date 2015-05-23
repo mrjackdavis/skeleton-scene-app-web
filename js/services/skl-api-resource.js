d12ed0c3067d945d1fe5cfdb0e3b63236bce8b31
Jack Davis <mr.jackdavis@gmail.com>
2015-05-23 11:19:54 +1000

    Revert API url back to production

--- js/services/skl-api-resource.js
+++ js/services/skl-api-resource.js
@@ -1,5 +1,5 @@
-// var SKL_API_URL = 'http://skl-api-426627428.ap-southeast-2.elb.amazonaws.com/v0-2';
-var SKL_API_URL = 'http://dockervm:8080/v0-2';
+var SKL_API_URL = 'http://skl-api-426627428.ap-southeast-2.elb.amazonaws.com/v0-2';
+// var SKL_API_URL = 'http://dockervm:8080/v0-2';
 
 angular.module('SceneSkeleton')
 	.factory("Scenes", function($resource) {
