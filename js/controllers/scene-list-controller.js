144df742ba689e09184679683df1c811b61e7a22
Jack Davis <mr.jackdavis@gmail.com>
2015-05-24 21:43:17 +1000

    Add auto update to list page

--- js/controllers/scene-list-controller.js
+++ js/controllers/scene-list-controller.js
@@ -1,32 +1,38 @@
 angular.module('SceneSkeleton')
-	.controller('SceneListController',function($scope,Scenes,SceneRequests){
-		$scope.scenes = [];
-		Scenes.query(function(data) {
-			$scope.rows = [];
-			$scope.totalScenes = data.length;
+	.controller('SceneListController',function($scope,$interval,Scenes,SceneRequests){
+		function updateSceneList(){
+			$scope.scenes = [];
+			Scenes.query(function(data) {
+				$scope.rows = [];
+				$scope.totalScenes = data.length;
 
-			var scenes =  data;
+				var scenes =  data;
 
-			var currentRow = [];
+				var currentRow = [];
 
-			scenes.forEach(function(scene){
-				if(currentRow.length >= 3){
+				scenes.forEach(function(scene){
+					if(currentRow.length >= 3){
+						$scope.rows.push(currentRow);
+						currentRow = [];
+					}
+					currentRow.push(scene);
+				});
+				if(currentRow.length > 0){
 					$scope.rows.push(currentRow);
-					currentRow = [];
 				}
-				currentRow.push(scene);
 			});
-			if(currentRow.length > 0){
-				$scope.rows.push(currentRow);
-			}
-		});
-		SceneRequests.query(function(data){
-			$scope.numInProgress = data.filter(function(request){
-				return request.status === 'IN_PROGRESS';
-			}).length;
+			SceneRequests.query(function(data){
+				$scope.numInProgress = data.filter(function(request){
+					return request.status === 'IN_PROGRESS';
+				}).length;
 
-			$scope.numPending = data.filter(function(request){
-				return request.status === 'PENDING';
-			}).length;
-		});
+				$scope.numPending = data.filter(function(request){
+					return request.status === 'PENDING';
+				}).length;
+			});
+		}
+		
+		updateSceneList();
+
+		$interval(updateSceneList, 3000);
 	});
