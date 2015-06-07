cf105da418a261ee793e5a822f128af037a88bcf
Jack Davis <mr.jackdavis@gmail.com>
2015-06-07 16:28:09 +1000

    Add more detail to display page

--- js/controllers/scene-display-controller.js
+++ js/controllers/scene-display-controller.js
@@ -2,5 +2,11 @@ angular.module('SceneSkeleton')
 	.controller('SceneDisplayController',function($scope,$routeParams,Scenes){
 		var scene = Scenes.get({id:$routeParams.sceneId,completedAt:$routeParams.completedAt},function(){
 			$scope.scene = scene;
+
+			var completeAtMoment = moment(scene.completedAt,'x');
+			var requestedAtMoment = moment(scene.requestedAt,'x');
+
+			$scope.timeSinceComplete = completeAtMoment.fromNow();
+			$scope.timeToGenerate = requestedAtMoment.to(completeAtMoment);
 		});
 	});
